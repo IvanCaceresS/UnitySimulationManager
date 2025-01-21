c1e49f9dd2d9d3848bb0c32ffeda9ed463781ccd
IvanCaceresS <icaceressatorres2001@gmail.com>
2025-01-20 21:01:22 -0300

    Adding DOTS_CUBES

---
+++ DOTS_CUBES/Assets/ECS/CubeSpawnerAuthoring.cs
@@ -0,0 +1,27 @@
+using Unity.Entities;
+using UnityEngine;
+
+namespace ECS
+{
+    public class CubeSpawnerAuthoring : MonoBehaviour
+    {
+        public GameObject prefab;
+        public float spawnRate;
+    }
+
+    class CubeSpawnerBaker : Baker<CubeSpawnerAuthoring>
+    {
+        public override void Bake(CubeSpawnerAuthoring authoring)
+        {
+            Entity entity = GetEntity(TransformUsageFlags.None);
+
+            AddComponent(entity, new CubeSpawnerComponent
+            {
+                prefab = GetEntity(authoring.prefab, TransformUsageFlags.Dynamic),
+                spawnPos = authoring.transform.position,
+                nextSpawnTime = 0.0f,
+                spawnRate = authoring.spawnRate
+            });
+        }
+    }
+}
---
+++ DOTS_CUBES/README.md
@@ -0,0 +1,17 @@
+# Primer proyecto de Unity DOTS ECS
+
+## Requisitos e intalaciones previas
+- Unity 2022.3.49f1
+- Paquetes a instalar
+    - Entities
+    - Entities Graphics
+    - Mathematics
+    - Burst
+## Hierarchy
+SampleScene
+    - Main Camera
+    - Directional Light
+    - Global Volume
+    - GameObject (Script 'DebugOverlay.cs')
+    - New Sub Scene
+        - CubeSpawner (Script 'CubeSpawnerAuthoring.cs')
