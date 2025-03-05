5adca3fe54af4fc7f6fe2aab7cb2a8c5f67454d4
IvanCaceresS <icaceressatorres2001@gmail.com>
2025-03-05 16:42:46 -0300

    ADDING NEW CODES

---
+++ SimulationManager/app/Simulations/FINE-TUNING-EX_13/Assets/Scripts/Components/EColi_2Component.cs
@@ -0,0 +1,9 @@
+using Unity.Entities;
+using Unity.Mathematics;
+public struct EColi_2Component : IComponentData
+{
+    public float TimeReference,MaxScale,GrowthTime,GrowthDuration,TimeSinceLastDivision,DivisionInterval,SeparationThreshold;
+    public bool HasGeneratedChild,IsInitialCell,TimeReferenceInitialized;
+    public Entity Parent;
+    public int SeparationSign;
+}
